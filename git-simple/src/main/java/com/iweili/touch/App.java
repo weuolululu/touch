fe7b474596d9dafe09580c1a034e471809bdb440
wei <weuolululu@hotmail.com>
2013-04-20 16:59:19 +0800

    init git-simple project

---
+++ git-simple/src/main/java/com/iweili/touch/App.java
@@ -0,0 +1,13 @@
+package com.iweili.touch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
