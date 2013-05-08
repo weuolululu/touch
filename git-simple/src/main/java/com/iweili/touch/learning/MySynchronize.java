3ab0fd64df43b90673e7367bf0c3a88e773b025b
wei-jeffrey.zhou <wei-jeffrey.zhou@autodesk.com>
2013-05-08 16:52:33 +0800

    finished MySynchronize

--- git-simple/src/main/java/com/iweili/touch/learning/MySynchronize.java
+++ git-simple/src/main/java/com/iweili/touch/learning/MySynchronize.java
@@ -8,7 +8,21 @@ package com.iweili.touch.learning;
  * To change this template use File | Settings | File Templates.
  *
  * Learning java synchronize.
+ *
+ * reference:
  * http://www.cnblogs.com/GnagWang/archive/2011/02/27/1966606.html
+ *
+ * highlight:
+ * 1． 定义private 的instance变量+它的 get方法，而不要定义public/protected的instance变量。如果将变量定义为public，对象在外界可以
+ *
+ * 绕过同步方法的控制而直接取得它，并改动它。这也是JavaBean的标准实现方式之一。
+ *
+ * 2． 如果instance变量是一个对象，如数组或ArrayList什么的，那上述方法仍然不安全，因为当外界对象通过get方法拿到这个instance对象
+ *
+ * 的引用后，又将其指向另一个对象，那么这个private变量也就变了，岂不是很危险。 这个时候就需要将get方法也加上synchronized同步，并
+ *
+ * 且，只返回这个private对象的clone()――这样，调用端得到的就是对象副本的引用了
+ *
  */
 public class MySynchronize implements Runnable {
 
@@ -82,17 +96,31 @@ public class MySynchronize implements Runnable {
 //    }
 //    //endregion
 
-    //region section 3
-    public void m4t2() {
-        synchronized (this) {
-            int i = 5;
-            while (i-- > 0) {
-                System.out.println(Thread.currentThread().getName() + ":" + i);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
+//    //region section 3
+//    public void m4t2() {
+//        synchronized (this) {
+//            int i = 5;
+//            while (i-- > 0) {
+//                System.out.println(Thread.currentThread().getName() + ":" + i);
+//                try {
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {
+//
+//                }
+//            }
+//        }
+//    }
+//    //endregion
 
-                }
+    //region section 4
+    public synchronized void m4t2() {
+        int i = 5;
+        while (i-- > 0) {
+            System.out.println(Thread.currentThread().getName() + ":" + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+
             }
         }
     }
