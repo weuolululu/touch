caf178025dba100aad4468f19c94bfbb978c76d2
wei-jeffrey.zhou <wei-jeffrey.zhou@autodesk.com>
2013-05-08 16:37:02 +0800

    finished MySynchronize

--- git-simple/src/main/java/com/iweili/touch/learning/MySynchronize.java
+++ git-simple/src/main/java/com/iweili/touch/learning/MySynchronize.java
@@ -6,15 +6,38 @@ package com.iweili.touch.learning;
  * Date: 5/8/13
  * Time: 4:15 PM
  * To change this template use File | Settings | File Templates.
+ *
  * Learning java synchronize.
+ * http://www.cnblogs.com/GnagWang/archive/2011/02/27/1966606.html
  */
 public class MySynchronize implements Runnable {
 
     public static void mainRun(String [] args){
-        MySynchronize mySynchronize = new MySynchronize();
+//        //region section 1
+//        MySynchronize mySynchronize = new MySynchronize();
+//
+//        Thread thread1 = new Thread(mySynchronize, "A");
+//        Thread thread2 = new Thread(mySynchronize, "B");
+//
+//        thread1.start();
+//        thread2.start();
+//        //endregion
 
-        Thread thread1 = new Thread(mySynchronize, "A");
-        Thread thread2 = new Thread(mySynchronize, "B");
+        final MySynchronize mySynchronize = new MySynchronize();
+        Runnable runnable1 = new Runnable() {
+            public void run() {
+                mySynchronize.m4t1();
+            }
+        };
+
+        Runnable runnable2 = new Runnable() {
+            public void run() {
+                mySynchronize.m4t2();
+            }
+        };
+
+        Thread thread1 = new Thread(runnable1, "t1");
+        Thread thread2 = new Thread(runnable2, "t2");
 
         thread1.start();
         thread2.start();
@@ -30,4 +53,49 @@ public class MySynchronize implements Runnable {
             }
         }
     }
+
+    public void m4t1(){
+        synchronized (this){
+            int i = 5;
+            while (i-- > 0){
+                System.out.println(Thread.currentThread().getName()+":"+i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e){
+
+                }
+            }
+        }
+    }
+
+//    //region section 2
+//    public void m4t2(){
+//        int i = 5;
+//        while (i-- > 0){
+//            System.out.println(Thread.currentThread().getName()+":"+i);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e){
+//
+//            }
+//        }
+//    }
+//    //endregion
+
+    //region section 3
+    public void m4t2() {
+        synchronized (this) {
+            int i = 5;
+            while (i-- > 0) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+
+                }
+            }
+        }
+    }
+    //endregion
+
 }
